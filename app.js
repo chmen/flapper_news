bd0028dac4d93db4e21de286fc20490c1c360c7e
potspov-hp <tony.potapov@gmail.com>
2018-09-13 18:31:39 +0300

    add state posts to display comments

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
