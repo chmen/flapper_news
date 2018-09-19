9cd3a9221833addb6eb7572ec8b86d0b642e8aa0
potspov-hp <tony.potapov@gmail.com>
2018-09-19 17:02:00 +0300

    add input form for comment

--- app.js
+++ app.js
@@ -35,6 +35,16 @@ function($stateProvider, $urlRouterProvider) {
 'posts',
 function($scope, $stateParams, posts){
   $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }])
 
 .controller('MainCtrl', [
