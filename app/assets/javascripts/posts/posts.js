cc0d717e83a26c9f0945f824ddd4860527106679
potspov-hp <tony.potapov@gmail.com>
2018-10-25 13:12:52 +0300

    fix legacy success method

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -20,8 +20,8 @@ function($http){
 
 
   o.create = function(post) {
-    return $http.post('/posts.json', post).success(function(data){
-      o.posts.push(data);
+    return $http.post('/posts.json', post).then(function(data){
+      o.posts.push(post);
     });
   };
 
